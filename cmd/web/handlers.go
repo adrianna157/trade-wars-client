859b70af6ce6ca4a771e984fa244083abe2ef4e5
MaxTheMitchell <rat2043@gmail.com>
2019-11-21 17:01:52 -0800

    added .env and cleaned up code

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,38 +1,26 @@
 package main
+
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 	"text/template"
 )
-func fileToByte(fileName string) (bytes []byte) {
-	file, _ := os.Open(fileName)
-	bytes, _ = ioutil.ReadAll(file)
-	return
-}
 
 func displayTemplateFile(w http.ResponseWriter, r *http.Request, pathToFile string) {
-	files := []string{		
+	files := []string{
 		pathToFile,
 		"./ui/html/base.layout.tmpl",
 		"./ui/html/footer.partial.tmpl",
 	}
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message and use
-	// the http.Error() function to send a generic 500 Internal Server Error
-	// response to the user.
+
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
-	// We then use the Execute() method on the template set to write the template
-	// content as the response body. The last parameter to Execute() represents any
-	// dynamic data that we want to pass in, which for now we'll leave as nil.
+
 	err = ts.Execute(w, nil)
 	if err != nil {
 		log.Println(err.Error())
@@ -50,7 +38,6 @@ func players(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		displayTemplateFile(w, r, "./ui/html/welcome-screen.tmpl")
 	case "POST":
-		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
 		if err := r.ParseForm(); err != nil {
 			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
@@ -66,44 +53,14 @@ func showNavigationScreen(w http.ResponseWriter, r *http.Request) {
 	displayTemplateFile(w, r, "./ui/html/navigation-screen.tmpl")
 }
 
-// Add a showSnippet handler function.
 func showTradeScreen(w http.ResponseWriter, r *http.Request) {
 	displayTemplateFile(w, r, "./ui/html/trade-screen.tmpl")
 }
 
-// Add a createSnippet handler function.
 func showChatScreen(w http.ResponseWriter, r *http.Request) {
 	displayTemplateFile(w, r, "./ui/html/chat-screen.tmpl")
 }
 
-// Add a showSnippet handler function.
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	// Extract the value of the id parameter from the query string and try to
-	// convert it to an integer using the strconv.Atoi() function. If it can't
-	// be converted to an integer, or the value is less than 1, we return a 404 page
-	// not found response.
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	// Use the fmt.Fprintf() function to interpolate the id value with our response
-	// and write it to the http.ResponseWriter.
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-}
-
-// Add a createSnippet handler function.
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		// If it's not, use the w.WriteHeader() method to send a 405 status
-		// code and the w.Write() method to write a "Method Not Allowed"
-		// response body. We then return from the function so that the
-		// subsequent code is not executed.
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", 405)
-		return
-	}
-
-	w.Write([]byte("Create a new snippet..."))
+func redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/players", 301)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,10 @@
 package main
+
 import (
 	"log"
 	"net/http"
 	"os"
 )
-
-// func redirect(w http.ResponseWriter, r *http.Request) {
-// 	http.Redirect(w, r, "http://localhost:"+getPort()+"/players", 301)
-// }
-func redirect(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/players", 301)
-}
 
 func getPort() string {
 	port := os.Getenv("PORT")
@@ -19,33 +13,20 @@ func getPort() string {
 	}
 	return port
 }
-func main() {
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
 
+func main() {
+
+	port := getPort()
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.HandleFunc("/players", players)
-	// mux.HandleFunc("/", redirect)
 	mux.HandleFunc("/", redirect)
 	mux.HandleFunc("/map", showNavigationScreen)
 	mux.HandleFunc("/map/trade", showTradeScreen)
 	mux.HandleFunc("/map/chat", showChatScreen)
-	mux.HandleFunc("/snippet/create", createSnippet)
-	mux.HandleFunc("/snippet", showSnippet)
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	// Use the http.ListenAndServe() function to start a new web server. We pass in
-	// two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit. Note
-	// that any error returned by http.ListenAndServe() is always non-nil.
-	log.Println("Starting server on " + getPort())
-	err := http.ListenAndServe(":"+getPort(), mux)
+
+	log.Println("Starting server on " + port)
+	err := http.ListenAndServe(":"+port, mux)
 	log.Fatal(err)
 }
